114f1c2137033b260ceaf3de5526fd118e8e9d6f
rna0 <47921907+rna0@users.noreply.github.com>
2020-02-08 21:50:14 +0200

    Add files via upload
    2 NEServer sending one photo

--- NEServer/NEServer/Form1.cs
+++ NEServer/NEServer/Form1.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -22,10 +21,72 @@ namespace NEServer
         public Form1()
         {
             InitializeComponent();
+            //send the bitmap to server as bytes
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                int port = 13000;
+                IPAddress localAddr = IPAddress.Parse(GetIp());//"127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                byte[] bytes = new byte[8192];
+                this.pictureBox1.Image = new Bitmap(@"E:\FINALS\RoyNES  working one controller\WindowsFormsApp0\bin\x86\Debug\image1.png");
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+
+                    // Get a stream object for reading and writing
+                    NetworkStream nwStream = client.GetStream();
+                    byte[] bytesToSend = ImageToByte();//ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+
+                    try
+                    {
+                        nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                    }
+                    catch (SocketException e1)
+                    {
+                        Console.WriteLine("SocketException: " + e1);
+                    }
+                    Console.WriteLine("image sent");
+
+
+                    client.Close();
+
+                    // Shutdown and end connection
+                }
+            }
+            catch (SocketException f)
+            {
+                Console.WriteLine("SocketException: {0}", f);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
         }
 
         private string GetIp()
         {
+            //gets ip via internet
             string localIP;
             using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
             {
@@ -37,76 +98,13 @@ namespace NEServer
         }
         public static byte[] ImageToByte(Image img = null)
         {
-            Image imag = new Bitmap(@"D:\Photos\MeshulamPro.jpg");
+            //convert my image into bytes for sending
+            Image imag = new Bitmap(@"C:\Users\royar\Pictures\1.jpg");
             ImageConverter converter = new ImageConverter();
             return (byte[])converter.ConvertTo(imag, typeof(byte[]));
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-                TcpListener server = null;
-                try
-                {
-                    // Set the TcpListener on port 13000.
-                    Int32 port = 13000;
-                IPAddress localAddr = IPAddress.Parse(GetIp());//"127.0.0.1");
-
-                    // TcpListener server = new TcpListener(port);
-                    server = new TcpListener(localAddr, port);
-
-                    // Start listening for client requests.
-                    server.Start();
-
-                    // Buffer for reading data
-                    Byte[] bytes = new Byte[8192];
-                    String data = null;
-
-                //---data to send to the server---
-                string textToSend = "hello world"; //DateTime.Now.ToString();
-                                                   // Enter the listening loop.
-                while (true)
-                    {
-                        Console.Write("Waiting for a connection... ");
-
-                        // Perform a blocking call to accept requests.
-                        // You could also user server.AcceptSocket() here.
-                        TcpClient client = server.AcceptTcpClient();
-                        Console.WriteLine("Connected!");
-
-                        data = null;
-
-                        // Get a stream object for reading and writing
-                        NetworkStream nwStream = client.GetStream();
-                    byte[] bytesToSend = ImageToByte();//ASCIIEncoding.ASCII.GetBytes(textToSend);
-
-                    //---send the text---
-                    Console.WriteLine("Sending : " + textToSend);
-                    nwStream.Write(bytesToSend, 0, bytesToSend.Length);
-
-                    //---read back the text---
-                    byte[] bytesToRead = new byte[client.ReceiveBufferSize];
-                    //int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
-                    //Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
-                    Console.ReadLine();
-
-
-
-                    // Shutdown and end connection
-                    client.Close();
-                    }
-                }
-                catch (SocketException f)
-                {
-                    Console.WriteLine("SocketException: {0}", f);
-                }
-                finally
-                {
-                    // Stop listening for new clients.
-                    server.Stop();
-                }
-
-
-                Console.WriteLine("\nHit enter to continue...");
-                Console.Read();
         }
     }
 }
